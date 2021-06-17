d98c2c4d13ac89e642d125e1b571eafc2418c877
Ismael E. Lastre Alvarez <ismaelfac@gmail.com>
2021-06-17 12:13:55 -0500

    cronometro and event ok

--- src/app/components/cronometro/cronometro.component.ts
+++ src/app/components/cronometro/cronometro.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-cronometro',
+  selector: 'cronometro',
   templateUrl: './cronometro.component.html',
   styleUrls: ['./cronometro.component.css']
 })
 export class CronometroComponent implements OnInit {
 
-  constructor() { }
+  contador: number;
+  interval: any;
+
+  constructor() { 
+    this.contador = 12;
+   }
 
   ngOnInit(): void {
+    
+  }
+
+  onStart($event:any) {
+    this.interval =  setInterval(() => {
+      this.contador--;
+      if(this.contador === 0) { clearInterval(this.interval) }
+    }, 1000);
+    console.log($event.target.id);
+  }
+
+  onReset($event:any) {
+    console.log($event.target);
+    if(this.contador <= 5) { 
+      clearInterval(this.interval);
+      this.contador = 12;
+    }
+  }
+
+  onChangeSelect($event:any) {
+    console.log($event.target.value);
   }
 
 }
